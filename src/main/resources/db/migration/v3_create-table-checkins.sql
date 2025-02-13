de146387f5a37cf82b11b889478f4fbdf6ba87ce
Matheus-AMoreira <matheus.moreira2172@gmail.com>
2025-02-12 21:40:55 -0300

    "Migrations"

---
+++ src/main/resources/db/migration/v3_create-table-checkins.sql
@@ -0,0 +1,7 @@
+create table check_ins (
+    id integer not nul primary key identity,
+    created_at timestamp default current_timestamp,
+    attendee_id varchar(255) not null,
+    constraint check_ins_attendee_id_fkey foreign key (attendee_id)
+    references attendees (id) on delete restrict on update cascade
+);
