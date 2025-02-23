fb2ac030c3af233736cad78c90c3d6d0953ae5f2
Matheus-AMoreira <matheus.moreira2172@gmail.com>
2025-02-23 18:18:21 -0300

    "EndPoint for register participant"

--- src/main/java/matheus/com/pass_in/controller/AttendeeController.java
+++ src/main/java/matheus/com/pass_in/controller/AttendeeController.java
@@ -1,10 +1,26 @@
 package matheus.com.pass_in.controller;
 
+import lombok.RequiredArgsConstructor;
+import matheus.com.pass_in.dto.attendee.AttendeeBadgeResponseDTO;
+import matheus.com.pass_in.services.AttendeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
+
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeBadgeResponseDTO response= this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(response);
+    }
 
 }
--- src/main/java/matheus/com/pass_in/controller/EventController.java
+++ src/main/java/matheus/com/pass_in/controller/EventController.java
@@ -1,7 +1,9 @@
 package matheus.com.pass_in.controller;
 
 import lombok.RequiredArgsConstructor;
+import matheus.com.pass_in.dto.attendee.AttendeeIdDTO;
 import matheus.com.pass_in.dto.attendee.AttendeeListResponseDTO;
+import matheus.com.pass_in.dto.attendee.AttendeeRequestDTO;
 import matheus.com.pass_in.dto.event.EventIdDTO;
 import matheus.com.pass_in.dto.event.EventRequestDTO;
 import matheus.com.pass_in.dto.event.EventResponseDTO;
@@ -16,23 +18,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequiredArgsConstructor
 public class EventController {
 
-    private final EventService service;
+    private final EventService eventService;
 
     private final AttendeeService attendeeService;
 
     @GetMapping("/{eventId}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId){
-        EventResponseDTO event = this.service.getEventDetail(eventId);
+        EventResponseDTO event = this.eventService.getEventDetail(eventId);
         return ResponseEntity.ok(event);
-    }
-
-    @PostMapping
-    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
-        EventIdDTO eventIdDTO = this.service.createEvent(body);
-
-        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.EventId()).toUri();
-
-        return ResponseEntity.created(uri).body(eventIdDTO);
     }
 
     @GetMapping("/attendees/{id}")
@@ -40,4 +33,23 @@ public class EventController {
         AttendeeListResponseDTO attendeeListResponseDTO = this.attendeeService.getEventsAttendee(id);
         return  ResponseEntity.ok(attendeeListResponseDTO);
     }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.EventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeIdDTO attendeeId = this.eventService.registerAttendeeOnEvent(body, eventId);
+
+        var uri = uriComponentsBuilder.path("attendees/{attendeeId}/badge").buildAndExpand(attendeeId.attendeeId()).toUri();
+
+        return ResponseEntity.created(uri).body(attendeeId);
+    }
+
 }
